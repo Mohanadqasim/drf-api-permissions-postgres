173cae2b795733530579871248504cf150964e9e
mohanadqasim <mohanad.qasim@hotmail.com>
2023-07-10 16:37:10 +0300

    done

--- movies/settings.py
+++ movies/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'movies.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",
-        "PORT": 5432,
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',  # or 'localhost'
+        'PORT': 5432,
     }
 }
 
